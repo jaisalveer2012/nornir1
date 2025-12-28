d28aa23b7d6a32f89ab37f9ea6c1f61f8a110df6
jasilveer2012 <jaisalveer2012@gmail.com>
2025-12-28 17:40:48 +0530

    structured_data_through_genie

---
+++ runbook8.py
@@ -0,0 +1,13 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def structured_output(task):
+    output = task.run(task=send_command, command="show version")
+    task.host["facts"] = output.scrapli_response.genie_parse_output()
+    print(task.host["facts"])
+
+results = nr.run(task=structured_output)
+# print_result(results)
