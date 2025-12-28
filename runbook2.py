cdf56305dd40273906a83d5a668280cb211a8543
jasilveer2012 <jaisalveer2012@gmail.com>
2025-12-28 16:08:58 +0530

    Send_config

---
+++ runbook2.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_config
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def send_config_test(task):
+    task.run(task=send_config, config="ntp server 1.1.1.1")
+
+results = nr.run(task=send_config_test)
+print_result(results)
