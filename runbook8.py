01348b56199a7fc89b9bf38dd2b0c04699b93bd9
jasilveer2012 <jaisalveer2012@gmail.com>
2026-01-02 22:24:57 +0530

    h

--- runbook8.py
+++ runbook8.py
@@ -7,7 +7,7 @@ nr = InitNornir(config_file="config.yaml")
 def structured_output(task):
     output = task.run(task=send_command, command="show version")
     task.host["facts"] = output.scrapli_response.genie_parse_output()
-    print(task.host["facts"])
+    # print(task.host["facts"])
 
 results = nr.run(task=structured_output)
-# print_result(results)
+print_result(results)
