0f4fce71416a5d5bdd9c22c86db6bedd46c9057c
jasilveer2012 <jaisalveer2012@gmail.com>
2026-01-03 11:18:22 +0530

    ipdb with structured data

--- runbook10.py
+++ runbook10.py
@@ -6,11 +6,12 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 def structured_output(task):
-    output = task.run(task=send_command, command="show ip int brief")
+    output = task.run(task=send_command, command="show ip interface")
     task.host["facts"] = output.scrapli_response.genie_parse_output()
-    print(task.host["facts"])
+    interfaces = task.host["facts"]["Loopback111"]
+    print(interfaces)
 
 results = nr.run(task=structured_output)
 # print_result(results)
-ipdb.set_trace()
+# ipdb.set_trace()
 
