514d5e59c31a0c21f1c09a1e09e8cb0c4020e8a0
jasilveer2012 <jaisalveer2012@gmail.com>
2026-01-03 12:56:55 +0530

    automating interface description

--- runbook11.py
+++ runbook11.py
@@ -2,14 +2,20 @@ from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
 import ipdb
+from rich import print as rprint
 
 nr = InitNornir(config_file="config.yaml")
 
 def structured_output(task):
     output = task.run(task=send_command, command="show cdp neighbors")
     task.host["facts"] = output.scrapli_response.genie_parse_output()
-    # print(task.host["facts"])
+    cdp_index = task.host["facts"]["cdp"]["index"]
+    for num in cdp_index:
+        local_interface = cdp_index[num]["local_interface"]
+        remote_device = cdp_index[num]["device_id"]
+        remote_port = cdp_index[num]["port_id"]
+        rprint(f"{task.host} {local_interface} is connected to {remote_device} {remote_port}")
 
 results = nr.run(task=structured_output)
-print_result(results)
-ipdb.set_trace()
+# print_result(results)
+# ipdb.set_trace()
