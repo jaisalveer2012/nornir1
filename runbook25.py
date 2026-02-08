fd67c1b75b7252c3dc9b86d8903ffb8d8f39cdb8
jasilveer2012 <jaisalveer2012@gmail.com>
2026-02-08 20:05:24 +0530

    mac searching

--- runbook25.py
+++ runbook25.py
@@ -1,3 +1,4 @@
+from math import inf
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
@@ -13,8 +14,32 @@ def find_mac_address(task):
     for interface in interfaces:
         mac_addr = interfaces[interface]["mac_address"]
         if target == mac_addr:
-            print(f"{task.host}'s {interface} has the mac address {mac_addr}")
+            intf = interface
+            print_info(task, intf)
+
+def print_info(task, intf):
+    print(f"MAC ADDRESS : {target} is present on {task.host} interface {intf}")
+    
+    cdp_response = task.run(task=send_command, command="show cdp neighbors")
+    task.host["cdpinfo"] = cdp_response.scrapli_response.genie_parse_output()
+    index = task.host["cdpinfo"]["cdp"]["index"]
+    for num in index:
+        local_intf = index[num]["local_interface"]
+        if local_intf == intf:
+            dev_id = index[num]["device_id"]
+            port_id = index[num]["port_id"]
+    # Get version and serial number
+    ver_result = task.run(task=send_command, command="show version")
+    task.host["verinfo"] = ver_result.scrapli_response.genie_parse_output()
+    version = task.host["verinfo"]["version"]
+    serial_num = version["chassis_sn"]
+    version_short = version["version_short"]
+    print(f"DEVICE VERSION : {version_short}")
+    print(f"DEVICE SERIAL NUMBER : {serial_num}")
+    
+
 
 nr.run(task=find_mac_address)
+# nr.run(task=cdp_neighbors)
 # import ipdb
 # ipdb.set_trace()
